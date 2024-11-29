33560b0f84ee208a75a1f58881467e46bc88468c
ferzian <m.ferzian09@gmail.com>
2024-11-29 10:42:29 +0700

    Add new style and new page

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/age', methods= ['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        born = int(request.form['born'])
+        now = datetime.now().year
+        age = now - born
+        return render_template('predict.html', age=age, born=born, now=now) 
+    return render_template('predict.html', age= None)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
